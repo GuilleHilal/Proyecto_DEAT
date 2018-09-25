b02337eab282a25dada2bf915dc932222a62ddd0
Ignacio <ignaciogarciagimenez@gmail.com>
2018-09-24 23:51:06 -0300

    Subo proyecto con implementacion US

---
+++ Producto/Código Fuente/Web/DEAT_Web_Proyecto/DeliveryEat/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DeliveryEat.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Producto/Código Fuente/Web/DEAT_Web_Proyecto/DeliveryEat/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DeliveryEat.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
